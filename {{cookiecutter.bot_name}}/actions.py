c02398bc1db66446564e90a6805f856da4d17ef1
Victor Vasiliev <victor@imperson.com>
2020-02-24 17:11:53 +0200

    Push source.

---
+++ {{cookiecutter.bot_name}}/actions.py
@@ -0,0 +1,38 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from coco_rasa import GenericCoCoAction
+
+
+class OneLiners(GenericCoCoAction):
+    # component name is the component_id from CoCo marketplace
+    component_name = "generic_oneliners_vp3"
+
+
+class Namer(GenericCoCoAction):
+    component_name = "namer_vp3"
+
+    def run(self):
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
